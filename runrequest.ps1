bb71390c25ec5c64014af2b395bd7084e58da8e1
unknown <james.dickson@comhem.se>
2016-02-01 22:31:54 +0100

    Added example file for powershell usage of the .net executable

---
+++ runrequest.ps1
@@ -0,0 +1,27 @@
+# This is just a sample of how to use the nurl .net-assembly for fuzzing-like tasks.
+param($strPath = "nurl.exe")
+
+# Create the nurl-object
+$assembly = [Reflection.Assembly]::LoadFile($strPath)
+$nurl = $assembly.CreateInstance("nurl.Nurl")
+
+# Just create some request-file used for tests
+echo "GET /INSERTNUMBERHERE HTTP/1.1" > requestExample.txt
+echo "Host: justMyHost.com" >> requestExample.txt
+
+# Example of simple automation
+
+$nurl.strFileName = "requestExample.txt"
+
+for ($i = 0 ; $i -lt 10 ; $i = $i + 1)
+{
+    $nurl.strHost = "127.0.0.1"
+    $nurl.bIsSSL = $false
+
+    $nurl.strReplacers.Clear()
+    $nurl.strReplacers.Add("INSERTNUMBERHERE")
+    $nurl.strReplacers.Add($i.ToString())
+
+    $nurl.run()
+}
+
