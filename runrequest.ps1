e047d6645d3ae5ee3b9dbec1ed9a6f28f409d8f3
unknown <james.dickson@comhem.se>
2016-02-01 22:35:59 +0100

    Added example file for powershell usage of the .net executable

--- runrequest.ps1
+++ runrequest.ps1
@@ -10,13 +10,13 @@ echo "GET /INSERTNUMBERHERE HTTP/1.1" > requestExample.txt
 echo "Host: justMyHost.com" >> requestExample.txt
 
 # Example of simple automation
-
 $nurl.strFileName = "requestExample.txt"
 
 for ($i = 0 ; $i -lt 10 ; $i = $i + 1)
 {
     $nurl.strHost = "127.0.0.1"
     $nurl.bIsSSL = $false
+    $nurl.bEchoWrite = $false
 
     $nurl.strReplacers.Clear()
     $nurl.strReplacers.Add("INSERTNUMBERHERE")
